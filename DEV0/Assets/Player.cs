f68403285a0c419d19be2106c0ce39aae87760f8
Ronno777 <smithryan954@gmail.com>
2025-01-17 17:50:11 -0700

    Update Player.cs

--- DEV0/Assets/Player.cs
+++ DEV0/Assets/Player.cs
@@ -17,10 +17,20 @@ public class Player : MonoBehaviour
         {
             transform.position += Vector3.forward * 0.5f;
         }
-        
+
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.back * 0.5f;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * 0.25f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * 0.25f;
         }
     }
 }
